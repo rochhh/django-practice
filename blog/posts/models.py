e999b9955153ce0d80bb077b96d38c1b696d0e08
Rochishnu Paliwal <rochishnu7@gmail.com>
2022-08-14 17:54:56 +0530

    made the title visible instead of it showing an 'object object'

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
+class Post(models.Model): 
+    
     title = models.CharField(max_length=25)
     body = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.title}"
